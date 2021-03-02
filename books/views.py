4c6d94d0c37bb87bd85ec5081ddb707ae9d56e68
timeofwise <shockingpark@gmail.com>
2021-03-02 22:26:47 +0900

    add books 1

--- books/views.py
+++ books/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def books(request):
+    return render(request, 'books/blog.html')
--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('home.urls'), name='home'),
-    path('books/', include('books.urls'), name='books'),
+    path('', include('home.urls')),
+    path('books/', include('books.urls')),
     path('admin/', admin.site.urls),
 ]+  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
