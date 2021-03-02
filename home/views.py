33110452ec80d3e30c1ec56291783767e79db413
timeofwise <shockingpark@gmail.com>
2021-03-02 21:43:59 +0900

    auto background

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 
 def home(request):
+    rand = random.randint(1,8)
 
-    return render(request, 'home/home.html')
+    return render(request, 'home/home.html', {
+        'rand' : rand,
+    })
