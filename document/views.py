41ee005a1a75c8ba0060858c3f084cc1177be01d
timeofwise <shockingpark@gmail.com>
2021-03-21 22:46:03 +0900

    static url

--- document/views.py
+++ document/views.py
@@ -5,10 +5,11 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 # Create your views here.
 
+number_files = len(fnmatch.filter(os.listdir('/home/busyangels/busy-angels/static/img/'), '*.jpg'))
+#number_files = len(fnmatch.filter(os.listdir('static/img/'), '*.jpg'))
+
 def storage(request):
     template = 'document/storage.html'
-
-    number_files = len(fnmatch.filter(os.listdir('static/img/'), '*.jpg'))
     rand = random.randint(1, number_files)
     scraps = Storage.objects.all()
 
